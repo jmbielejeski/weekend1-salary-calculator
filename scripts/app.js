36f6d52c30838ee9455d33c952769613d504222d
jmbielejeski <jmbielejeski@gmail.com>
2021-01-29 15:01:21 -0700

    started to add functionality to submit button

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,20 @@
 console.log('JS is running');
+
+$(document).ready(onReady);
+
+let employeeData = [];
+
+function onReady() {
+  console.log('I am ready');
+
+  $(document).on('click', '#submitBtn', onSubmit);
+  // add submit button functionality
+  // take date and append to employeeData
+  // render to the DOM
+}
+
+// write onSubmit function
+function onSubmit(event) {
+  event.preventDefault();
+  console.log('in onSubmit');
+}
